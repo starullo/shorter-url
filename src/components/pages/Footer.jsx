c5356cb34e6964de78e7b4202f6db965c28fb37d
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-09-23 19:32:40 -0500

    basic component structure

---
+++ src/components/pages/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+<div className='footer'>
+  <div>
+    <h3>
+  Features</h3>
+
+  Link Shortening
+  Branded Links
+  Analytics
+    </div>
+
+    <div>
+  <h3>Resources</h3>
+
+  Blog
+  Developers
+  Support
+  </div>
+
+  <div>
+  <h3>Company</h3>
+
+  About
+  Our Team
+  Careers
+  Contact
+  </div>
+</div>
+  
+    )
+}
+
+export default Footer
---
+++ src/components/pages/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav>
+        <div>
+          <ul>
+            <li><a href='#'>Features</a></li>
+            <li><a href='#'>Pricing</a></li>
+            <li><a href='#'>Resources</a></li>
+          </ul>
+        </div>
+        <div>
+          <li><a href='#'>Login</a></li>
+          <li><a href='#'>Sign Up</a></li>
+        </div>
+      </nav>
+    )
+}
+
+export default Nav
