45dfabac4013ca1f8d1687ec88ce9d4afa801e07
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-09-24 19:21:07 -0500

    add react from dependency

--- src/components/pages/Short.jsx
+++ src/components/pages/Short.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {useForm} from 'react-hook-form';
 
 const Short = () => {
+    const {handleSubmit, register, errors} = useForm();
+    const onSub = input => {
+        console.log(input)
+    }
+
     return (
         <div>
-        <input type='text'/>
-        <button>Shorten It!</button>
+            <input 
+            type='text' 
+            name='urlShort' 
+            placeholder='Enter your URL' 
+            ref={register({
+                required: 'Required',
+                pattern: {
+                    // value: '',
+                    message: 'please enter a URL',
+                }
+            })}/>
+            {errors.urlShort && errors.urlShort.message}
+            <button type='submit'>Shorten It!</button>
         </div>
     )
 }
